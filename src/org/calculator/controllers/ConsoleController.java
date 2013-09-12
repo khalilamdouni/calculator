7dce917a46701f4eb455100476df64005bfde51f
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-09-12 21:18:27 +0100

    Controller implemented

--- src/org/calculator/controllers/ConsoleController.java
+++ src/org/calculator/controllers/ConsoleController.java
@@ -1,6 +1,17 @@
 package org.calculator.controllers;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.calculator.models.IAlgorithme;
+import org.calculator.models.impl.ConsoleModel;
+import org.calculator.models.impl.HeapSortAlgo;
+import org.calculator.models.impl.InsertionSortAlgo;
+import org.calculator.models.impl.MergeSortAlgo;
+import org.calculator.models.impl.Result;
+import org.calculator.models.impl.SelectionSortAlgo;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,9 +28,29 @@ public class ConsoleController {
 	
 	@RequestMapping(value = "/calculate", method = RequestMethod.GET)
 	public ModelAndView calculate() {
-		return null;
+		ConsoleModel consoleModel = new ConsoleModel();
+		consoleModel.setAlgos(new ArrayList<IAlgorithme>());
+		consoleModel.getAlgos().add(new HeapSortAlgo());
+		consoleModel.getAlgos().add(new InsertionSortAlgo());
+		consoleModel.getAlgos().add(new MergeSortAlgo());
+		consoleModel.getAlgos().add(new SelectionSortAlgo());
+		return new ModelAndView("console", "consoleModel", consoleModel);
 	}
 	
-	
+	@RequestMapping(value = "/calculate", method = RequestMethod.POST)
+	public ModelAndView calculate(ConsoleModel consoleModel, BindingResult result) {
+		Random rand = new Random();
+		consoleModel.setResults(new ArrayList<Result>());
+		consoleModel.getResults().add(new Result(10, rand.nextInt(10)));
+		consoleModel.getResults().add(new Result(20, rand.nextInt(20)));
+		consoleModel.getResults().add(new Result(30, rand.nextInt(30)));
+		consoleModel.getResults().add(new Result(40, rand.nextInt(40)));
+		consoleModel.getResults().add(new Result(50, rand.nextInt(50)));
+		consoleModel.getResults().add(new Result(60, rand.nextInt(60)));
+		consoleModel.getResults().add(new Result(70, rand.nextInt(70)));
+		consoleModel.getResults().add(new Result(80, rand.nextInt(80)));
+		consoleModel.getResults().add(new Result(90, rand.nextInt(90)));
+		return new ModelAndView("console", "consoleModel", consoleModel);
+	}
 	
 }
