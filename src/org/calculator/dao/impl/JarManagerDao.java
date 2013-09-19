9c767c9a523682995179dbd2862dbb3cb439a3ef
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-09-20 00:09:49 +0100

    JPA with hibernate implementation added Datasource, Entity manager factory configured

--- src/org/calculator/dao/impl/JarManagerDao.java
+++ src/org/calculator/dao/impl/JarManagerDao.java
@@ -4,15 +4,32 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
 import org.calculator.dao.IJarManagerDao;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+@Repository("jarManagerDao")
+@Transactional
 public class JarManagerDao implements IJarManagerDao {
 
+	private static final Logger logger = Logger.getLogger(JarManagerDao.class);
+	
+	@PersistenceContext
+	private EntityManager em;
+	
 	@Override
 	public String saveJar(MultipartFile jarFile) throws IllegalStateException,
 			IOException {
 
+		for (String key : em.getProperties().keySet()) {
+			logger.info("===> " + key + " : " + em.getProperties().get(key));
+		}
+		
 		File jarLocalFile = new File("/home/khalil/work/spring/jartest/"
 				+ UUID.randomUUID().toString() + ".jar");
 		jarFile.transferTo(jarLocalFile);
