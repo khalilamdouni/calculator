0c5d3d7203825ec12258c7e7db82f2d275efba5c
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-10-04 20:39:14 +0100

    Methods and Params management controller implemented

--- src/org/calculator/controllers/ReflectorController.java
+++ src/org/calculator/controllers/ReflectorController.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.calculator.business.IClassManager;
 import org.calculator.business.IJarManager;
+import org.calculator.business.IMethodManager;
+import org.calculator.business.IParamManager;
 import org.calculator.models.CalculatorClass;
+import org.calculator.models.CalculatorClassMethod;
+import org.calculator.models.CalculatorMethodParam;
 import org.calculator.models.ReflectorModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -22,8 +26,11 @@ public class ReflectorController {
 
 	private IJarManager jarManager;
 	private IClassManager classManager;
-	
-	private static final Logger logger = Logger.getLogger(ReflectorController.class);
+	private IMethodManager methodManager;
+	private IParamManager paramManager;
+
+	private static final Logger logger = Logger
+			.getLogger(ReflectorController.class);
 
 	@RequestMapping(value = "/reflector", method = RequestMethod.GET)
 	public ModelAndView getReflector() {
@@ -43,14 +50,42 @@ public class ReflectorController {
 		return new ModelAndView("classForm", "classModel",
 				classManager.getCalculatorClass(classId));
 	}
-	
+
 	@RequestMapping(value = "/saveClass", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saveClass(@RequestBody CalculatorClass claculatorClass) {
 		logger.info("Save off : " + claculatorClass);
 		return new ModelAndView("classForm", "classModel",
 				this.classManager.saveCalculatorClass(claculatorClass));
 	}
-	
+
+	@RequestMapping(value = "/getMethodForm/{methodId}")
+	public ModelAndView getMethodForm(@PathVariable("methodId") long methodId) {
+		return new ModelAndView("methodForm", "methodModel",
+				this.methodManager.getMethod(methodId));
+	}
+
+	@RequestMapping(value = "/saveMethod", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ModelAndView saveMethod(
+			@RequestBody CalculatorClassMethod calculatorClassMethod) {
+		logger.info("Save off : " + calculatorClassMethod);
+		return new ModelAndView("methodForm", "methodModel",
+				this.methodManager.saveMethod(calculatorClassMethod));
+	}
+
+	@RequestMapping(value = "/getParamForm/{paramId}")
+	public ModelAndView getParamForm(@PathVariable("paramId") long paramId) {
+		return new ModelAndView("paramForm", "paramModel",
+				this.paramManager.getParam(paramId));
+	}
+
+	@RequestMapping(value = "/saveParam", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ModelAndView saveParam(
+			@RequestBody CalculatorMethodParam calculatorMethodParam) {
+		logger.info("Save off : " + calculatorMethodParam);
+		return new ModelAndView("paramForm", "paramModel",
+				this.paramManager.saveParam(calculatorMethodParam));
+	}
+
 	public IJarManager getJarManager() {
 		return jarManager;
 	}
@@ -69,6 +104,26 @@ public class ReflectorController {
 	@Qualifier(value = "classManager")
 	public void setClassManager(IClassManager classManager) {
 		this.classManager = classManager;
+	}
+
+	public IMethodManager getMethodManager() {
+		return methodManager;
+	}
+
+	@Autowired
+	@Qualifier(value = "methodManager")
+	public void setMethodManager(IMethodManager methodManager) {
+		this.methodManager = methodManager;
+	}
+
+	public IParamManager getParamManager() {
+		return paramManager;
+	}
+
+	@Autowired
+	@Qualifier(value = "paeamManager")
+	public void setParamManager(IParamManager paramManager) {
+		this.paramManager = paramManager;
 	}
 
 }
