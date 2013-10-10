caf346cc05435f0625aadcabbefac69ca1cbd7cc
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-10-10 21:11:02 +0100

    JPA entity for execution plan implemented

---
+++ src/org/calculator/models/ExecutionPlan.java
@@ -0,0 +1,58 @@
+package org.calculator.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "EXEC_PLAN")
+public class ExecutionPlan {
+	
+	@Id
+	@Column(name = "ID")
+	private long id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	@Column(name = "SEQUENCE")
+	private String sequence;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+	
+}
