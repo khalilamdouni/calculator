7573001f30386587dc6136bc7efa82131bab17aa
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-10-27 15:42:21 +0100

    code formatted

--- src/org/calculator/business/IJarScenarioManager.java
+++ src/org/calculator/business/IJarScenarioManager.java
@@ -6,7 +6,7 @@ import org.calculator.models.JarScenario;
 
 /**
  * The interface of the jar scenario manager which is responsible for managing
- * scenarios of java methods methods
+ *  execution scenarios of java methods methods
  * 
  * @author khalil.amdouni
  * 
